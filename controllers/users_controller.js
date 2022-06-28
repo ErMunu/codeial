bc396bf0ed69c8dfeb1b7ea520df2a0b0abddb63
ErMunu <me@muntazir.in>
2022-06-28 22:50:01 +0530

    signin and signup basics

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,28 @@ module.exports.profile = function(req,res){
         title: "My Profile"
     })
 }
+
+//render signup
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "User Sign Up"
+    })
+}
+
+//render signin
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "User Sign in"
+    })
+}
+
+// get signup data
+module.exports.create = function(req,res){
+    //todo later
+    }
+
+
+// get signin data
+module.exports.createSession = function(req,res){
+    //todo later
+    }
