19fff69187a6ab8c92a1a07f3f720fde4f42a492
ErMunu <me@muntazir.in>
2022-06-29 07:30:29 +0530

    signup done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
     return res.render('profile',{
         title: "My Profile"
@@ -20,7 +22,29 @@ module.exports.signIn = function(req,res){
 
 // get signup data
 module.exports.create = function(req,res){
-    //todo later
+        if (req.body.password != req.body.confirm_password){
+            console.log('password not match');
+            return res.redirect('back');
+        }
+        
+        User.findOne({email: req.body.email}, function(err, user){
+            if(err){
+                console.log('error in finding user')
+                return;
+            }
+
+            if(!user){
+                User.create(req.body, function(err, user){
+                    if(err){
+                        console.log('Cannot create user : '+ err)
+                    }
+
+                    return res.redirect('users/sign-in');
+                });
+            } else {
+                return res.redirect('back');
+            }
+        } )
     }
 
 
