c3e8ade60bd2e5e614fdb53cae322869dfdd3652
ErMunu <me@muntazir.in>
2022-07-09 21:49:34 +0530

    integrated redis with kue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,7 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
-
+const commentEmailWorker = require('../workers/comment_email_worker');
+const queue = require('../config/kue');
 
 module.exports.create = async function (req, res) {
     try {
@@ -17,7 +18,14 @@ module.exports.create = async function (req, res) {
             post.save();
 
             comment = await comment.populate('user', 'name email');
-            commentsMailer.newComment(comment);
+            //commentsMailer.newComment(comment);
+            let job = queue.create('emails', comment).save(function (err) {
+                if (err) {
+                    console.log('error creating job', err);
+                    return;
+                }
+                console.log('Job enqueued',job.id);
+            });
 
             if (req.xhr) {
                 return res.status(200).json({
